e1f7540a55cca62d9e9a35877054bb3d357b7328
Alexey Gochachilov <agochachilov@gmail.com>
2025-12-30 12:17:02 +0300

    clean code

--- src/main/java/ru/aston/finalproject/service/FileWriter.java
+++ src/main/java/ru/aston/finalproject/service/FileWriter.java
@@ -1,15 +1,13 @@
 package ru.aston.finalproject.service;
 
 import lombok.AllArgsConstructor;
-import ru.aston.finalproject.entity.User;
 import ru.aston.finalproject.parser.Parsing;
-import ru.aston.finalproject.parser.UserParser;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.List;
 
@@ -19,10 +17,6 @@ public class FileWriter<T> {
     private final Parsing<T> parser;
 
     public void write(List<T> list, String filePath) {
-        // TODO: код для записи в файл в режиме добавления данных
-        // для текстирования класса создать объект класса и передать в него парсер
-        // пример: UserParser userParser = new UserParser();
-        // пример: FileWriter<User> listSorter = new FileWriter<>(userParser);
 
         Iterator<T> iterator = list.iterator();
         StringBuilder content = new StringBuilder();
@@ -32,11 +26,10 @@ public class FileWriter<T> {
         }
         try (BufferedWriter writer = new BufferedWriter(
                 new OutputStreamWriter(new FileOutputStream(filePath, true),
-                        "UTF-8"))) {
+                        StandardCharsets.UTF_8))) {
             writer.write(content.toString());
         } catch (IOException e) {
             System.out.println("Error writing to file: " + e.getMessage());
         }
     }
-
 }
