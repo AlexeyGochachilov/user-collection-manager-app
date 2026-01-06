33b747311969685c10775f2b3a3f9771bb02ec68
Lich <lonedarkwolf.09@gmail.com>
2026-01-06 16:50:03 +0300

    I made edits in the sorting

--- src/main/java/ru/aston/finalproject/service/ListSorter.java
+++ src/main/java/ru/aston/finalproject/service/ListSorter.java
@@ -1,14 +1,51 @@
 package ru.aston.finalproject.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListSorter<T extends Comparable<T>> {
+    private List<T> list;
 
-    public List<T> sort(List<T> list) {
-        // TODO: реализовать базовую сортировку тут
-        // для текстирования класса создать объект класса
-        // пример: ListSorter<User> listSorter = new ListSorter<>();
-        // listSorter.sort(передать список нужный)
-        return list;
+    public ListSorter(List<T> list) {
+        this.list = list;
+        mergeSort(this.list);
+    }
+
+    private void mergeSort(List<T> list) {
+        if (list.size() <= 1) return;
+
+        int mid = list.size() / 2;
+        List<T> left = new ArrayList<>(list.subList(0, mid));
+        List<T> right = new ArrayList<>(list.subList(mid, list.size()));
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(list, left, right);
+    }
+
+    private void merge(List<T> result, List<T> left, List<T> right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i).compareTo(right.get(j)) <= 0) {
+                result.set(k++, left.get(i++));
+            } else {
+                result.set(k++, right.get(j++));
+            }
+        }
+        while (i < left.size()) {
+            result.set(k++, left.get(i++));
+        }
+        while (j < right.size()) {
+            result.set(k++, right.get(j++));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
