7b1b77c5244bd55a7efee15a84d3fd4480e0e27b
Seamly71 <nikrush7766@gmail.com>
2026-01-07 14:03:51 +0700

    Добавил контракт сортировщика + написал доп задание 1.

---
+++ src/main/java/ru/aston/finalproject/service/sorting/SillySorter.java
@@ -0,0 +1,41 @@
+package ru.aston.finalproject.service.sorting;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class SillySorter {
+
+    public <ListItemT> List<ListItemT> sort(
+            List<ListItemT> list,
+            Function<ListItemT, Integer> getIntegerField
+    ) {
+        List<ListItemT> evens = new ArrayList<>();
+        List<Integer> sequentialPositionsOfEvens = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            ListItemT reference = list.get(i);
+            if (
+                    getIntegerField.apply(reference) % 2 == 0
+            ) {
+                evens.add(reference);
+                sequentialPositionsOfEvens.add(i);
+            }
+        }
+
+        // TODO: Сортировка четных, мне нужен код Алексея, пока из утила.
+        evens.sort(
+                Comparator.comparing(getIntegerField)
+        );
+
+        for (int i = 0; i < sequentialPositionsOfEvens.size(); i++) {
+            list.set(
+                    sequentialPositionsOfEvens.get(i),
+                    evens.get(i)
+            );
+        }
+
+        return list;
+    }
+}
