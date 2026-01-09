80fa1acbd302c4c57f69e297ef91cceb5c44627d
Lich <lonedarkwolf.09@gmail.com>
2026-01-09 16:54:47 +0300

    С помощью Никиты отредактировал код под нужды проекта

--- src/main/java/ru/aston/finalproject/service/ListSorter.java
+++ src/main/java/ru/aston/finalproject/service/ListSorter.java
@@ -1,36 +1,44 @@
 package ru.aston.finalproject.service;
 
+import ru.aston.finalproject.service.sorting.Sorter;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-public class ListSorter<T extends Comparable<T>> {
-    private List<T> list;
-
-    public ListSorter(List<T> list) {
-        this.list = list;
-        mergeSort(this.list);
-    }
-
-    private void mergeSort(List<T> list) {
-        if (list.size() <= 1) return;
+public class ListSorter extends Sorter {
+    public <ListItemT> List<ListItemT> sort(
+            List<ListItemT> list,
+            Comparator<ListItemT> comparator
+    ) {
+        if (list.size() <= 1) {
+            return list;
+        }
 
         int mid = list.size() / 2;
-        List<T> left = new ArrayList<>(list.subList(0, mid));
-        List<T> right = new ArrayList<>(list.subList(mid, list.size()));
+        List<ListItemT> left = new ArrayList<>(list.subList(0, mid));
+        List<ListItemT> right = new ArrayList<>(list.subList(mid, list.size()));
 
-        mergeSort(left);
-        mergeSort(right);
+        sort(left, comparator);
+        sort(right, comparator);
 
-        merge(list, left, right);
+        merge(list, left, right, comparator);
+
+        return list;
     }
 
-    private void merge(List<T> result, List<T> left, List<T> right) {
+    private <ListItemT> void merge(
+            List<ListItemT> result,
+            List<ListItemT> left,
+            List<ListItemT> right,
+            Comparator<ListItemT> comparator
+    ) {
         int i = 0;
         int j = 0;
         int k = 0;
 
         while (i < left.size() && j < right.size()) {
-            if (left.get(i).compareTo(right.get(j)) <= 0) {
+            if (comparator.compare(left.get(i), right.get(j)) <= 0) {
                 result.set(k++, left.get(i++));
             } else {
                 result.set(k++, right.get(j++));
@@ -42,10 +50,5 @@ public class ListSorter<T extends Comparable<T>> {
         while (j < right.size()) {
             result.set(k++, right.get(j++));
         }
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
     }
 }
