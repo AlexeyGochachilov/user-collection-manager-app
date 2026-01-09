44454f2dc9e98fa1b71154a7d9a4ca000786df57
Seamly71 <nikrush7766@gmail.com>
2026-01-09 20:24:46 +0700

    Сейв от 09.01.

---
+++ src/main/java/ru/aston/finalproject/service/counters/MultithreadCounter.java
@@ -0,0 +1,66 @@
+package ru.aston.finalproject.service.counters;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MultithreadCounter {
+    public <ListItemT> int count(
+           List<ListItemT> list,
+           ListItemT target,
+           int threadCount
+    ) throws ExecutionException, InterruptedException {
+        if (threadCount < 1) {
+            throw new IllegalArgumentException(
+                   "Expected thread count to be a positive integer."
+            );
+        }
+
+        int step = list.size() / threadCount;
+        if (threadCount == 1 || step < 1) {
+           return count(list, target);
+        }
+
+        ExecutorService tasks = Executors.newFixedThreadPool(threadCount);
+        List<Future<Integer>> results = new ArrayList<>();
+        for (int multiplier = 0; multiplier < threadCount - 1; multiplier++) {
+            int finalMultiplier = multiplier;
+            results.add(tasks.submit(
+                    () -> count(
+                            list.subList(step * finalMultiplier, step * (finalMultiplier + 1)),
+                            target
+                    )
+            ));
+        }
+        results.add(tasks.submit(
+                () -> count(
+                        list.subList(step * (threadCount - 1) , list.size()),
+                        target
+                )
+        ));
+
+        int returnValue = 0;
+        for (Future<Integer> value : results) {
+            returnValue += value.get();
+        }
+
+        tasks.close();
+        return returnValue;
+    }
+
+    public <ListItemT> int count(
+           List<ListItemT> list,
+           ListItemT target
+    ) {
+        int count = 0;
+        for (ListItemT item : list) {
+            if (item.equals(target)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
