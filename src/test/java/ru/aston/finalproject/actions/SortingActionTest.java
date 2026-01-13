27a4dd44662f1af170d08945f028a18c9ffd893e
Lich <lonedarkwolf.09@gmail.com>
2026-01-13 17:50:01 +0300

    add SortingActionTest

--- src/test/java/ru/aston/finalproject/actions/SortingActionTest.java
+++ src/test/java/ru/aston/finalproject/actions/SortingActionTest.java
@@ -16,8 +16,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 class SortingActionTest {
@@ -51,6 +57,7 @@ class SortingActionTest {
                 User.builder().setName("Bob").setEmail("bob@example.com").setAge(30).build()
         );
     }
+
     @Nested
     @DisplayName("Basic sorting")
     class BasicSorting {
